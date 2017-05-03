1aafcfd0cd429912c7bc1af8a6115dc1a275c5f4
Benjamin Efron <Benjamin Efron>
2017-05-03 04:37:06 -0400

    .

--- server/remote_server.py
+++ server/remote_server.py
@@ -42,7 +42,7 @@ def toggle():
 
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect(('137.112.104.162', 8089))
-    print (type(toggle_state))
+    print (type(toggle_state.encode('utf-8')))
     clientsocket.send(str.encode(toggle_state))
 
     return jsonify({'result' : 'success'})
